f115da3a6ae608ede7cf30ebc2c5fdaffe7eea52
swaminipatil <swaminipatil8767@gmail.com>
2026-06-10 19:44:27 +0530

    day9 changes

---
+++ day-9/jsondemo.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "swamini"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "swamini",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
